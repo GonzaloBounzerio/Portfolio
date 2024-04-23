31838ac9027d8228afe43435f6aace2b77cea7cd
GonzaloBounzerio <bounzeriog@gmail.com>
2024-04-22 23:54:38 -0300

    ss

--- src/components/common/cards/proyectCard/ProyectCard.jsx
+++ src/components/common/cards/proyectCard/ProyectCard.jsx
@@ -1,7 +1,10 @@
 
+import { useEffect, useState } from "react"
 import "./ProyectCard.css"
 
 const ProyectCard = () => {
+
+
   return (
     <>
         <div className="bodyProyectCard">
@@ -9,8 +12,7 @@ const ProyectCard = () => {
                 <h2>Lorem ipsum</h2>
             </div>
             <div className="imgProyectCard">
-                <img id="fotoFondo" src="https://res.cloudinary.com/duvugx7r9/image/upload/v1711672266/proyectoPortfolio/lataPepsi2_uggqd7.jpg"/>
-                <img src="https://res.cloudinary.com/duvugx7r9/image/upload/v1711671985/proyectoPortfolio/lataPepsi1_ru0uq6.jpg" id="fotoHover"/>
+                <img src="https://res.cloudinary.com/duvugx7r9/image/upload/v1711671985/proyectoPortfolio/lataPepsi1_ru0uq6.jpg" alt="" />
             </div>
             <div className="divTextHover">
                 <h4 id="textHover">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
